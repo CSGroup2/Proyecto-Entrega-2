03a67ec8960a067715123920b147defcf9517f4d
hlbv17 <helenlbv17@gmail.com>
2022-02-19 14:40:18 -0500

    validación ambulancia

--- Control/Validacion.cs
+++ Control/Validacion.cs
@@ -2,8 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace Control {
     public class Validacion {
+
+        // Método que valida las entradas de datos en módulo ambulancias y lanza un error visible para el usuario
+        public bool Validar(TextBox txtPlaca, TextBox txtModelo, ComboBox cmbTipo, TextBox txtCapacidad, TextBox txtObservacion, ErrorProvider errorP)
+        {
+            bool no_error = true;
+            string placa = txtPlaca.Text;
+            if (String.IsNullOrEmpty(txtPlaca.Text.Trim()))
+            {
+                errorP.SetError(txtPlaca, "Ingrese la placa");
+                no_error = false;
+            }
+            if (!Regex.IsMatch(placa, "^[A-Z]{3}[0-9]{3}$") || !Regex.IsMatch(placa, "^[A-Z]{3}[0-9]{4}$") || dAmbulancia.ConsultarPlaca(txtPlaca.Text))
+            {
+                errorP.SetError(txtPlaca, "La placa ya está registrada\nLa placa debe contener de 3 letras y 3-4 dígitos");
+                no_error = false;
+            }
+            if (String.IsNullOrEmpty(txtModelo.Text))
+            {
+                errorP.SetError(txtModelo, "Ingrese el modelo de la ambulancia");
+                no_error = false;
+            }
+            if (cmbTipo.Text == "--Seleccionar--")
+            {
+                errorP.SetError(cmbTipo, "Seleccione un tipo de ambulancia");
+                no_error = false;
+            }
+            if (String.IsNullOrEmpty(txtCapacidad.Text.Trim()))
+            {
+                errorP.SetError(txtCapacidad, "Ingrese la capacidad de la ambulancia");
+                no_error = false;
+            }
+            if (Int32.Parse(txtCapacidad.Text) < 0 || Int32.Parse(txtCapacidad.Text) > 20)
+            {
+                errorP.SetError(txtCapacidad, "Ingrese un dato real");
+                no_error = false;
+            }
+            if (String.IsNullOrEmpty(txtObservacion.Text))
+            {
+                errorP.SetError(txtObservacion, "Ingrese detalles de la ambulancia");
+                no_error = false;
+            }
+            return no_error;
+        }
     }
 }
