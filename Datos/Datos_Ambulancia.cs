bd3c539adb3c550c142bbd4d456cf0d77cfbeaa2
hlbv17 <helenlbv17@gmail.com>
2022-02-19 14:48:35 -0500

    método insertar ambulancias

---
+++ Datos/Datos_Ambulancia.cs
@@ -0,0 +1,77 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+    public class Datos_Ambulancia
+    {
+        Conexion con = new Conexion();
+
+        //INSERTA REGISTROS DE AMBULANCIAS
+        public String InsertarAmbulancia(Ambulancia a)
+        {
+            string msj = "";
+            SqlConnection c1 = con.abrir_conexion();
+            try
+            {
+                if (a != null)
+                {
+                    //establecer transaccion
+                    //comando
+                    SqlCommand comando = new SqlCommand();
+                    comando.Connection = c1;
+                    comando.CommandText = "sp_insertar_ambulancia";
+                    comando.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter param_placa = new SqlParameter();
+                    param_placa.ParameterName = "@placa";
+                    param_placa.SqlDbType = SqlDbType.VarChar;
+                    param_placa.Value = a.Placa;
+                    comando.Parameters.Add(param_placa);
+
+                    SqlParameter param_modelo = new SqlParameter();
+                    param_modelo.ParameterName = "@modelo";
+                    param_modelo.SqlDbType = SqlDbType.VarChar;
+                    param_modelo.Value = a.Modelo;
+                    comando.Parameters.Add(param_modelo);
+
+                    SqlParameter param_tipo_ambulancia = new SqlParameter();
+                    param_tipo_ambulancia.ParameterName = "@tipo_ambulancia";
+                    param_tipo_ambulancia.SqlDbType = SqlDbType.Int;
+                    param_tipo_ambulancia.Value = a.Tipo_ambulancia;
+                    comando.Parameters.Add(param_tipo_ambulancia);
+
+                    SqlParameter param_capacity = new SqlParameter();
+                    param_capacity.ParameterName = "@capacidad";
+                    param_capacity.SqlDbType = SqlDbType.Int;
+                    param_capacity.Value = a.Capacidad;
+                    comando.Parameters.Add(param_capacity);
+
+                    SqlParameter param_observacion = new SqlParameter();
+                    param_observacion.ParameterName = "@capacidad";
+                    param_observacion.SqlDbType = SqlDbType.VarChar;
+                    param_observacion.Value = a.Observacion;
+                    comando.Parameters.Add(param_observacion);
+
+                    msj = comando.ExecuteNonQuery() == 1 ? "Ok" : "No se ingreso el registro";
+                }    
+            }
+            catch (Exception ex)
+            {
+                con.cerrar_conexion(c1);
+                msj = "OCURRIO UN ERROR " + ex.Message;
+            }
+            return msj;
+        }
+
+        public bool ConsultarPlaca(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
