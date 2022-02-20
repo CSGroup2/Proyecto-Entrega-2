01b9d4ab8b93f311235e79d2a028235aab38f4d0
bethsaida03 <56984950+bethsaida03@users.noreply.github.com>
2022-02-20 14:49:53 -0500

    alex

---
+++ Datos/Datos_Persona2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos
+{
+    public class Datos_Persona2
+    {
+        Conexion con = new Conexion();
+
+
+    }
+}
