385bcc02033f4d60a927b880a58bffcba0e7a24f
bethsaida03 <56984950+bethsaida03@users.noreply.github.com>
2022-02-20 14:57:52 -0500

    cliente

--- Model/Cliente.cs
+++ Model/Cliente.cs
@@ -24,6 +24,14 @@ namespace Model
             this.usuario = usuario;
         }
 
+        public Cliente(int id_cliente, int id_hospital, Usuario2 usuario, int id_persona, string cedula, string nombre_1, string nombre_2, string apellido_1, string apellido_2, string sexo, DateTime fecha_nac, string telefono) : base(id_persona, cedula, nombre_1, nombre_2, apellido_1, apellido_2, sexo, fecha_nac, telefono)
+        {
+            this.id_cliente = id_cliente;
+            this.id_hospital = id_hospital;
+            this.usuario = usuario;
+        }
+
+
         public int Id_cliente { get => id_cliente; set => id_cliente = value; }
         public int Id_hospital { get => id_hospital; set => id_hospital = value; }
         public Usuario2 Usuario { get => usuario; set => usuario = value; }
