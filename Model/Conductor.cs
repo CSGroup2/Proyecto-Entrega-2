de7a4501f34bf0ec8a04a525f05a1f17bb877f4a
Renán Pérez Balladares <64661058+ropbGitHub@users.noreply.github.com>
2022-02-19 22:09:17 -0500

    ...

--- Model/Conductor.cs
+++ Model/Conductor.cs
@@ -4,40 +4,36 @@ using System.Linq;
 using System.Text;
 
 namespace Model {
-    public class Conductor {
+    public class Conductor : Persona {
 
         // Variables
         private int id_conductor;
-        private Persona persona;
         private DateTime fecha_contrato;
         private string diponibilidad;
-        
+
         // Constructor: Default
-        public Conductor () {
+        public Conductor () : base () {
             this.id_conductor = 0;
-            this.persona = null;
             this.fecha_contrato = new DateTime ();
             this.diponibilidad = "";
         }
 
         // Constructor: Parameterized
-        public Conductor (int id_conductor, Persona persona, DateTime fecha_contrato, string diponibilidad) {
+        public Conductor (int id_conductor, DateTime fecha_contrato, string diponibilidad, int id_persona, string cedula, string nombre_1, string nombre_2, string apellido_1, string apellido_2, string sexo, DateTime fecha_nac, string telefono) : base (id_persona, cedula, nombre_1, nombre_2, apellido_1, apellido_2, sexo, fecha_nac, telefono) {
             this.id_conductor = id_conductor;
-            this.persona = persona;
             this.fecha_contrato = fecha_contrato;
             this.diponibilidad = diponibilidad;
         }
 
         // Getters & Setter
         public int Id_conductor { get => id_conductor; set => id_conductor = value; }
-        public Persona Persona { get => persona; set => persona = value; }
         public DateTime Fecha_contrato { get => fecha_contrato; set => fecha_contrato = value; }
         public string Diponibilidad { get => diponibilidad; set => diponibilidad = value; }
 
         // ToString
         public override string ToString () {
-            return 
-                persona.ToString() +
+            return
+                base.ToString () +
                 "\r\nFecha de Contrato: " + fecha_contrato +
                 "\r\nDisonibilidad: " + diponibilidad;
         }
---
+++ Model/Secretaria.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model {
+    public class Secretaria : Persona{
+
+        // Variables
+        private Usuario usuario;
+
+        public Secretaria () : base () {
+            this.usuario = null;
+        }
+
+        public Secretaria (Usuario usuario, int id_persona, string cedula, string nombre_1, string nombre_2, string apellido_1, string apellido_2, string sexo, DateTime fecha_nac, string telefono) : base (id_persona, cedula, nombre_1, nombre_2, apellido_1,  apellido_2, sexo, fecha_nac, telefono) {
+            this.usuario = usuario;
+        }
+
+        public Usuario Usuario { get => usuario; set => usuario = value; }
+
+        public override string ToString () {
+            return
+                base.ToString ()+
+                usuario.ToString();
+        }
+    }
+}
