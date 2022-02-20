c5ff2e9c232e30228e8f78b1b0253c7f09230237
Renán Pérez Balladares <64661058+ropbGitHub@users.noreply.github.com>
2022-02-20 11:47:24 -0500

    Algo por ahí <Conductor Guardar>

--- Control/Adm_Conductor.cs
+++ Control/Adm_Conductor.cs
@@ -1,9 +1,99 @@
-﻿using System;
+﻿using Datos;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace Control {
-    class Adm_Conductor {
+    public class Adm_Conductor {
+
+        /*
+           Aplicar el patron de diseño Singleton:
+           1. Crear 1 atributo estático y privado de la misma clase.
+           2. Cambiar el constructor de public a private.
+           3.1. Crear un getter del atributo estático del tipo de la clase.
+           3.2.Verificar si atributo es null, si es así, crearlo.
+       */
+
+        private static Adm_Conductor adm = null;    // 1.
+        Validacion v = null;
+        Conductor conductor = null;
+
+        //public Usuario_Cache UsuarioCache { get => usuarioCache; set => usuarioCache = value; }
+
+        // Constructor: Adm_Login
+        private Adm_Conductor () {                  // 2.
+            conductor = new Conductor ();
+            v = new Validacion ();
+        }
+
+        // Getter: GetAdm
+        public static Adm_Conductor GetAdm () {     // 3.1.
+            if (adm == null) {                  //3.2
+                adm = new Adm_Conductor ();
+            }
+            return adm;
+        }
+
+        /*---------------------Frm_Conductor_Consultar---------------------------------*/
+
+        #region Frm_Conductor_Consultar
+
+        #endregion
+
+        /*---------------------Frm_Conductor_Editar---------------------------------*/
+
+        #region Frm_Conductor_Editar
+
+        #endregion
+
+        /*---------------------Frm_Conductor_Registrar---------------------------------*/
+
+        #region Frm_Conductor_Registrar
+
+        // Methods for KeyPress
+        public void Leer_Numeros_KeyPress (object sender, KeyPressEventArgs e) {
+            // Only allows numbers on press
+            v.Validar_Numeros_KeyPress (sender, e);
+        }
+
+        public void Leer_Lettras_KeyPress (object sender, KeyPressEventArgs e) {
+            // Only allows alphabetic characters
+            v.Validar_Letras_KeyPress (sender, e);  
+        }
+
+        public void Leer_Correo_Keypress (object sender, KeyPressEventArgs e) {
+            // Only allows eamil characters
+            v.Validar_Correo_KeyPress (sender, e);
+        }
+
+        // Methods for Buttons
+        public void Guardar (TextBox txt_Cedula, TextBox txt_Nombre1, TextBox txt_Nombre2, TextBox txt_Apellido1, TextBox txt_Apellido2, TextBox txt_Correo, TextBox txt_Telefono, RadioButton rdb_Masculino, RadioButton rdb_Femenino, DateTimePicker dtp_FechaNac, DateTimePicker dtp_FechaContrato, ErrorProvider errorProvider1) {
+            errorProvider1.Clear ();
+            if (v.EsCorrectoGuardar (txt_Cedula, txt_Nombre1, txt_Nombre2, txt_Apellido1, txt_Apellido2, txt_Correo, txt_Telefono, rdb_Masculino, rdb_Femenino, dtp_FechaNac, dtp_FechaContrato, errorProvider1)) {
+
+            }
+        }
+
+        public void LimpiarCampos (TextBox txt_Cedula, TextBox txt_Nombre1, TextBox txt_Nombre2, TextBox txt_Apellido1, TextBox txt_Apellido2, TextBox txt_Correo, TextBox txt_Telefono, RadioButton rdb_Masculino, RadioButton rdb_Femenino, DateTimePicker dtp_FechaNac, DateTimePicker dtp_FechaContrato, ErrorProvider errorProvider1) {
+            errorProvider1.Clear ();
+            txt_Cedula.Clear ();
+            txt_Nombre1.Clear ();
+            txt_Nombre2.Clear ();
+            txt_Apellido1.Clear ();
+            txt_Apellido2.Clear ();
+            txt_Correo.Clear ();
+            txt_Telefono.Clear ();
+            rdb_Masculino.Checked = false;
+            rdb_Femenino.Checked = false;
+            dtp_FechaNac.Value = DateTime.Today;
+            dtp_FechaContrato.Value = DateTime.Today;
+        }
+        #endregion
+
+
+
     }
 }
---
+++ Datos/Datos_Secretaria.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Datos {
+    public class Datos_Secretaria {
+
+    }
+}
