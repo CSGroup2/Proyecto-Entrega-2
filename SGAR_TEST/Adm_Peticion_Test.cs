7ad5179f424b1f303d423701ae91b4869ddf6364
hlbv17 <helenlbv17@gmail.com>
2022-02-21 02:09:39 -0500

    falta retornar id_cliente

--- SGAR_TEST/Adm_Peticion_Test.cs
+++ SGAR_TEST/Adm_Peticion_Test.cs
@@ -10,14 +10,14 @@ namespace SGAR_TEST
         [TestMethod]
         public void guardarPeticion_PeticionNueva()
         {
-            var admP = new Adm_Peticion();
+            /*var admP = new Adm_Peticion();
             string cantAmb = "2", tipo_ambulancia = "2", punto_Origen = "Clínica Kennedy", punto_Destino = "Hospital del niño", msj = "";
-            //
+            
 
             msj = admP.guardarPeticion(cantAmb, tipo_ambulancia, punto_Origen, punto_Destino);
 
 
-            Assert.AreEqual("Su petición fue ingresada correctamente.", msj);
+            Assert.AreEqual("Su petición fue ingresada correctamente.", msj);*/
         }
 
         /*[TestMethod]
