5d76ee0a1423a06c0cc62ac1994d3beb9f3f8889
Renán Pérez Balladares <64661058+ropbGitHub@users.noreply.github.com>
2022-02-21 19:35:52 -0500

    ...D':

--- Datos/Conexion.cs
+++ Datos/Conexion.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace Datos {
     public class Conexion {
         public SqlConnection abrir_conexion () {
-            string ConnectionString = "server=localhost; database=PYCSLosrapidos ; integrated security = true";
+            
+            /*
+            string ConnectionString = "";
+
+            XmlTextReader xtr = new XmlTextReader (@"hostlocal.xml"); // "" realatie path
+            while (xtr.Read()) {
+                if (xtr.NodeType == XmlNodeType.Element && xtr.Name == "name") {
+                    ConnectionString = xtr.ReadElementString ();
+                }
+            }
+            */
+            
+            string ConnectionString = "server=localhost; database=PYCSLosrapidos; integrated security=true";
             SqlConnection conexion = new SqlConnection (ConnectionString);
             conexion.Open ();
             //MessageBox.Show("SE ABRIO LA CONEXION DESDE LA CAPA ACCESO A DATOS");
@@ -19,5 +32,6 @@ namespace Datos {
             conexion.Close ();
             //MessageBox.Show("SE CERRO LA CONEXION DESDE LA CAPA ACCESO A DATOS");
         }
+
     }
 }
